2aa2dac238ded4da810bb1e9741f487f159c51b6
Ravi Choudhary <ravi1102726@gmail.com>
2024-09-30 23:21:20 +0530

    feat: add supp for lang translation

--- gradio-app/app.py
+++ gradio-app/app.py
@@ -4,10 +4,70 @@ import time
 import ollama
 from transformers import pipeline
 import numpy as np
+import chromadb
+
+from langdetect import detect
+from translate import Translator
 
 # history = [["user_msg_1", "bot_msg_1"], ["user_msg_2", "bot_msg_2"]]
 
+MODEL = "llama3.1:8b"
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
+hi_2_en_translator = Translator(from_lang="hi", to_lang="en")
+
+
+chromadb_client = chromadb.HttpClient(host="chromadb-vecdb", port=8000)
+collection = chromadb_client.get_collection(name="docs")
+ollama_client = ollama.Client(host='http://ollama:11434')
+
+def retrieve_context(prompt):
+    # generate an embedding for the prompt and retrieve the most relevant doc
+    prompt_embedding = ollama_client.embeddings(
+        prompt=prompt,
+        model=MODEL
+    )
+    # get relevant document
+    rag_contexts = collection.query(
+        query_embeddings=[prompt_embedding["embedding"]],
+        n_results=2
+    )
+    # build the rag context string
+    rag_context_str = "\n".join(rag_contexts['documents'][0])
+    return rag_context_str, rag_contexts
+
+
+def rag_bot(history):
+    print(f"bot history: {history}")
+    client = ollama_client#ollama.Client(host='http://ollama:11434')
+    messages = []
+    for human, ai in history[:-1]:
+        if human:
+            messages.append({"role": "user", "content": human})
+        if ai:
+            messages.append({"role": "assistant", "content": ai})
+
+    # get the user prompt from history
+    prompt = history[-1][0]
+    rag_context_str, rag_contexts = retrieve_context(prompt)
+    print(f"rag_context: {rag_contexts}")
+    # append the last user message with rag context
+    messages.append(
+        {
+            'role': 'user',
+            'content': f"""Given the following context: {rag_context_str}, 
+                        provide a concise and direct response to this prompt: {prompt}.
+                        Limit your answer to a maximum of 50 words.
+                        Do not provide explanations unless explicitly asked.""",
+        }
+    )
+    
+    stream = client.chat(model=MODEL, stream=True, messages=messages)
+    history[-1][1] = ""
+    for chunk in stream:
+        if chunk["message"]["content"] is not None:
+            history[-1][1] += chunk["message"]["content"]
+            yield history
+    
 
 def transcribe(audio):
     sr, y = audio
@@ -23,7 +83,7 @@ def transcribe(audio):
 
 def bot(history):
     print(f"bot history: {history}")
-    client = ollama.Client(host='http://ollama:11434')
+    client = ollama_client#ollama.Client(host='http://ollama:11434')
     messages = []
     for human, ai in history[:-1]:
         if human:
@@ -37,7 +97,7 @@ def bot(history):
             "role": "user", "content": history[-1][0]
         }
     )
-    stream = client.chat(model="phi:latest", stream=True, messages=messages)
+    stream = client.chat(model=MODEL, stream=True, messages=messages)
 
     history[-1][1] = ""
     for chunk in stream:
@@ -47,7 +107,7 @@ def bot(history):
 
 
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
+    chatbot = gr.Chatbot(height=550)
     # msg = gr.Textbox()
     msg = gr.MultimodalTextbox(interactive=True,
                                       file_count="multiple",
@@ -57,6 +117,13 @@ with gr.Blocks() as demo:
 
     def user(user_message, history, audio_input):
         print(f"user_msg: {user_message}")
+        if user_message['text']:
+            user_lang = detect(user_message['text'])
+            print(f"user_lang: {user_lang}")
+            if user_lang == 'hi':
+                # translate to eng
+                user_message['text'] = hi_2_en_translator.translate(user_message['text'])
+
         if not user_message["text"] and audio_input:
             user_message["text"] = transcribe(audio_input)
             audio_input = None
@@ -74,7 +141,7 @@ with gr.Blocks() as demo:
     #         yield history
 
     msg.submit(user, [msg, chatbot, audio_input], [msg, chatbot], queue=False).then(
-        bot, chatbot, chatbot
+        rag_bot, chatbot, chatbot
     )
     clear.click(lambda: None, None, chatbot, queue=False)
 
